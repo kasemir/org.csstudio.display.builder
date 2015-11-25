643c927e0631401273bc2440ad7463860d48be3c
kasemir <kasemirk@ornl.gov>
2015-11-25 15:13:01 -0500

    Icon lookup

--- org.csstudio.display.builder.util/src/org/csstudio/display/builder/util/Icons.java
+++ org.csstudio.display.builder.util/src/org/csstudio/display/builder/util/Icons.java
@@ -9,6 +9,7 @@ package org.csstudio.display.builder.util;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.net.URL;
 
 /** Helper for loading icons
  *  @author Kay Kasemir
@@ -21,11 +22,19 @@ public class Icons
      */
     public static InputStream getStream(final String path) throws Exception
     {
-        // TODO Handle "plugin://.." type path for icons inside Eclipse plugin
-        // This only works for tests that execute in the 'main' directory of a plugin,
-        // so "../" leads to the parent of all pluging sources, from which we
-        // then locate "specific_plugin/dir/file.png"
-        final String resolved = path.replace("platform:/plugin/", "../");
-        return new FileInputStream(resolved);
+        if (! path.startsWith("platform:"))
+            throw new Exception("Only handling 'platform:' path");
+        try
+        {
+            return new URL(path).openStream();
+        }
+        catch (Exception ex)
+        {
+            // Handle "platform://.." path during tests in the 'main' directory of a plugin,
+            // so "../" leads to the parent of all plugin sources, from which we
+            // then locate "specific_plugin/dir/file.png"
+            final String resolved = path.replace("platform:/plugin/", "../");
+            return new FileInputStream(resolved);
+        }
     }
 }
