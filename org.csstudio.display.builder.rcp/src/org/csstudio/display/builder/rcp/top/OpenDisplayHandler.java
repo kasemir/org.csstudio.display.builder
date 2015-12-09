837ae5089f2ae6b730bcd5a728b871137309aedf
kasemir <kasemirk@ornl.gov>
2015-12-08 19:01:31 -0500

    E4 Top displays...

--- org.csstudio.display.builder.rcp/src/org/csstudio/display/builder/rcp/top/OpenDisplayHandler.java
+++ org.csstudio.display.builder.rcp/src/org/csstudio/display/builder/rcp/top/OpenDisplayHandler.java
@@ -1,15 +1,13 @@
 package org.csstudio.display.builder.rcp.top;
 
-import javax.inject.Named;
-
 import org.eclipse.e4.core.di.annotations.Execute;
 
 // E4 Handler to open a display
 public class OpenDisplayHandler
 {
 	@Execute
-	public void execute(@Named("command.parameter.display") String display)
+	public void execute(/* @Named("display") String display */)
 	{
-	    System.out.println("Should open " + display);
+	    System.out.println("Should open some display");
 	}
 }
--- org.csstudio.display.builder.rcp/src/org/csstudio/display/builder/rcp/top/TopDisplaysMenuContribution.java
+++ org.csstudio.display.builder.rcp/src/org/csstudio/display/builder/rcp/top/TopDisplaysMenuContribution.java
@@ -12,14 +12,23 @@ import org.eclipse.e4.core.commands.ECommandService;
 import org.eclipse.e4.core.commands.EHandlerService;
 import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.ui.di.AboutToShow;
-import org.eclipse.e4.ui.model.application.ui.menu.MDirectMenuItem;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.commands.MCommand;
+import org.eclipse.e4.ui.model.application.ui.menu.MHandledMenuItem;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenu;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenuElement;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenuFactory;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
 
-// E4 DdynamicMenuContribution
-public class FavouriteDisplays
+// E4 DynamicMenuContribution
+public class TopDisplaysMenuContribution
 {
+    @Inject
+    MApplication application;
+
+    @Inject
+    EModelService model_service;
+
     @Inject
     ECommandService command_service;
 
@@ -53,24 +62,21 @@ public class FavouriteDisplays
 
     private void addMenuItem(MMenu menu, DisplayInfo display)
     {
-        final MDirectMenuItem item = MMenuFactory.INSTANCE.createDirectMenuItem();
+        // TODO: Unclear how to pass the DisplayInfo to a command parameter
+        final MCommand command = model_service.createModelElement(MCommand.class);
+        command.setElementId("org.csstudio.display.builder.rcp.command.open_display");
+
+//        final MParameter parameter = model_service.createModelElement(MParameter.class);
+//        parameter.setElementId("org.csstudio.display.builder.rcp.commandparameter.display");
+//        parameter.setName("display");
+//        parameter.setValue(display.getPath());
+
+        final MHandledMenuItem item = MMenuFactory.INSTANCE.createHandledMenuItem();
         item.setLabel(display.getName());
         item.setIconURI("platform:/plugin/org.csstudio.display.builder.rcp/icons/display.png");
+        item.setCommand(command);
+//        item.getParameters().add(parameter);
 
         menu.getChildren().add(item);
-
-        // TODO: Unclear how to tie these menu items to a command that
-        //       then receives the DisplayInfo
-
-//      MHandledMenuItem item2 = MMenuFactory.INSTANCE.createHandledMenuItem();
-//      item2.setLabel("Display 2");
-//      item2.setIconURI("platform:/plugin/org.csstudio.display.builder.rcp/icons/display.png");
-
-//
-//      Map<String, Object> parameters = new HashMap<>();
-//      parameters.put("command.parameter.display", "/some/path");
-//      ParameterizedCommand command = command_service.createCommand("org.csstudio.display.builder.rcp.command.open_display", parameters);
-//      item2.setCommand(command);
-//      menu.getChildren().add(item2);
     }
 }
