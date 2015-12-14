b5f3da4be67948f4bc41bef05e5470564d245a1f
kasemir <kasemirk@ornl.gov>
2015-12-14 09:13:54 -0500

    Adjust pom persions, update setup for another computer

--- build/setup_laptop.sh
+++ build/setup_laptop.sh
@@ -1,7 +1,17 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_65.jdk/Contents/Home
-export M2_HOME=$HOME/Eclipse/apache-maven-3.3.3
+if [ -d $HOME/Eclipse/jdk1.8.0_60 ]
+then
+    export JAVA_HOME=$HOME/Eclipse/jdk1.8.0_60
+else
+    export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_65.jdk/Contents/Home
+fi
+if [ -d $HOME/Eclipse/apache-maven-3.2.3 ]
+then
+    export M2_HOME=$HOME/Eclipse/apache-maven-3.2.3
+else
+    export M2_HOME=$HOME/Eclipse/apache-maven-3.3.3
+fi
 
 export PATH="$M2_HOME/bin:$JAVA_HOME/bin:$PATH"
 
-export CSS_REPO=file:/Users/ky9/git/org.csstudio.sns/repository/target/repository
+export CSS_REPO=file:$HOME/git/org.csstudio.sns/repository/target/repository
 
