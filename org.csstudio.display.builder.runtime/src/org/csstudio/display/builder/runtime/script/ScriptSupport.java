5ab07812c5a3ef21df73e709a7c03fe6f2acf644
kasemir <kasemirk@ornl.gov>
2016-01-11 12:44:00 -0500

    Scripts: Replace PVUtil package hack with patching the script text

--- org.csstudio.display.builder.runtime/src/org/csstudio/display/builder/runtime/script/ScriptSupport.java
+++ org.csstudio.display.builder.runtime/src/org/csstudio/display/builder/runtime/script/ScriptSupport.java
@@ -7,9 +7,14 @@
  *******************************************************************************/
 package org.csstudio.display.builder.runtime.script;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.ThreadFactory;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.csstudio.display.builder.model.properties.ScriptInfo;
 import org.csstudio.display.builder.model.util.NamedDaemonPool;
@@ -59,11 +64,46 @@ public class ScriptSupport
      */
     public Script compile(final String path, final InputStream stream) throws Exception
     {
+        final InputStream script_stream = patchScript(path, stream);
         if (ScriptInfo.isJython(path))
-            return jython.compile(path, stream);
+            return jython.compile(path, script_stream);
         else if (ScriptInfo.isJavaScript(path))
-            return javascript.compile(path, stream);
+            return javascript.compile(path, script_stream);
         throw new Exception("Cannot compile '" + path + "'");
+    }
+
+    /** Update legacy package names
+     *  @param path Name of script (file name, URL)
+     *  @param stream Stream for the script content
+     *  @return Patched stream
+     *  @throws Exception on error
+     */
+    private InputStream patchScript(final String path, final InputStream stream) throws Exception
+    {
+        boolean warned = false;
+
+        final StringBuilder buf = new StringBuilder();
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        String line;
+        while ((line = reader.readLine()) != null)
+        {
+            if (line.contains("scriptUtil"))
+            {
+                if (! warned)
+                {
+                    Logger.getLogger(getClass().getName())
+                          .log(Level.SEVERE,
+                               "Script '" + path + "' accessed deprecated org.csstudio.opibuilder.scriptUtil, " +
+                               "update to org.csstudio.display.builder.runtime.script.PVUtil");
+                    warned = true;
+                }
+                line = line.replace("org.csstudio.opibuilder.scriptUtil", "org.csstudio.display.builder.runtime.script");
+            }
+            buf.append(line).append('\n');
+        }
+        stream.close();
+
+        return new ByteArrayInputStream(buf.toString().getBytes());
     }
 
     /** Release resources (interpreter, ...) */
