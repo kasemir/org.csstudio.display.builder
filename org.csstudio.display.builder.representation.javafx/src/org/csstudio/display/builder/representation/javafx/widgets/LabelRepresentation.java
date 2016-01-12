a1bf9bc220d144acd863a79a98b22b04367a5884
kasemir <kasemirk@ornl.gov>
2016-01-12 18:09:49 -0500

    label: Represent horizontal alignment

--- org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/widgets/LabelRepresentation.java
+++ org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/widgets/LabelRepresentation.java
@@ -13,11 +13,11 @@ import org.csstudio.display.builder.model.widgets.LabelWidget;
 import org.csstudio.display.builder.representation.javafx.JFXUtil;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 
 /** Creates JavaFX item for model widget
@@ -27,6 +27,7 @@ public class LabelRepresentation extends RegionBaseRepresentation<Label, LabelWi
 {
     private final DirtyFlag dirty_style = new DirtyFlag();
     private final DirtyFlag dirty_content = new DirtyFlag();
+    private Pos pos;
 
     @Override
     public Label createJFXNode() throws Exception
@@ -44,11 +45,24 @@ public class LabelRepresentation extends RegionBaseRepresentation<Label, LabelWi
         model_widget.displayBackgroundColor().addUntypedPropertyListener(this::styleChanged);
         model_widget.displayTransparent().addUntypedPropertyListener(this::styleChanged);
         model_widget.displayFont().addUntypedPropertyListener(this::styleChanged);
+        model_widget.displayHorizontalAlignment().addUntypedPropertyListener(this::styleChanged);
         model_widget.displayText().addUntypedPropertyListener(this::contentChanged);
+    }
+
+    private Pos computePos()
+    {
+        switch (model_widget.displayHorizontalAlignment().getValue())
+        {
+        case CENTER: return Pos.TOP_CENTER;
+        case RIGHT:  return Pos.TOP_RIGHT;
+        default:
+        case LEFT:   return Pos.TOP_LEFT;
+        }
     }
 
     private void styleChanged(final WidgetProperty<?> property, final Object old_value, final Object new_value)
     {
+        pos = computePos();
         dirty_style.mark();
         toolkit.scheduleUpdate(this);
     }
@@ -68,19 +82,18 @@ public class LabelRepresentation extends RegionBaseRepresentation<Label, LabelWi
             Color color = JFXUtil.convert(model_widget.displayForegroundColor().getValue());
             jfx_node.setTextFill(color);
 
+            jfx_node.setPrefSize(model_widget.positionWidth().getValue(),
+                                 model_widget.positionHeight().getValue());
+            jfx_node.setAlignment(pos);
             if (model_widget.displayTransparent().getValue())
-            {   // No fill, auto-size
-                jfx_node.setPrefSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+            {   // No fill
                 jfx_node.setBackground(null);
             }
             else
-            {   // Fill correctly sized background
-                jfx_node.setPrefSize(model_widget.positionWidth().getValue(),
-                                     model_widget.positionHeight().getValue());
+            {   // Fill background
                 color = JFXUtil.convert(model_widget.displayBackgroundColor().getValue());
                 jfx_node.setBackground(new Background(new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY)));
             }
-
             jfx_node.setFont(JFXUtil.convert(model_widget.displayFont().getValue()));
         }
         if (dirty_content.checkAndClear())
