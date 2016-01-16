22deb21fba15c31e728a8cd6317993708d449b17
Kay Kasemir <kasemirk@ornl.gov>
2016-01-16 10:08:45 -0500

    Add WidgetInfoDialog

--- org.csstudio.display.builder.rcp/src/org/csstudio/display/builder/rcp/run/WidgetInfoAction.java
+++ org.csstudio.display.builder.rcp/src/org/csstudio/display/builder/rcp/run/WidgetInfoAction.java
@@ -11,16 +11,16 @@ import java.util.Collection;
 
 import org.csstudio.display.builder.model.Widget;
 import org.csstudio.display.builder.rcp.Plugin;
+import org.csstudio.display.builder.representation.javafx.WidgetInfoDialog;
 import org.csstudio.display.builder.runtime.WidgetRuntime;
 import org.csstudio.vtype.pv.PV;
-import org.diirt.vtype.VType;
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /** Action that displays information about a widget
  *  @author Kay Kasemir
  */
+@SuppressWarnings("nls")
 public class WidgetInfoAction extends Action
 {
     final Widget widget;
@@ -35,28 +35,8 @@ public class WidgetInfoAction extends Action
     public void run()
     {
         final WidgetRuntime<?> runtime = WidgetRuntime.ofWidget(widget);
-
-        // Text dump of widget info
-        final StringBuilder buf = new StringBuilder();
-        buf.append("Widget Name: ").append(widget.getName()).append("\n");
-        buf.append("Widget Type: ").append(widget.getType()).append("\n");
-        buf.append("\n");
         final Collection<PV> pvs = runtime.getPVs();
-        if (! pvs.isEmpty())
-        {
-            buf.append("PVs:\n");
-            for (PV pv : pvs)
-            {
-                buf.append(pv.getName()).append(" - ");
-                final VType value = pv.read();
-                if (value == null)
-                    buf.append("Disconnected\n");
-                else
-                    buf.append(value).append("\n");
-            }
-        }
-        // XXX Should display in dialog out of which user can copy/paste the text
-        // Could also use a fancier dialog with sections for each PV etc.
-        MessageDialog.openInformation(null, "Widget Info", buf.toString());
+        WidgetInfoDialog dialog = new WidgetInfoDialog(widget, pvs);
+        dialog.show();
     }
 }
