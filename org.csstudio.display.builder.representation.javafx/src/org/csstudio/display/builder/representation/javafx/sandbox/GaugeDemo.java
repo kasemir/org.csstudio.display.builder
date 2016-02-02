66cfed99e936144cee1ec8c9c7219f148a745606
Megan Grodowitz <grodowitzml@ornl.gov>
2016-02-02 15:16:43 -0500

    Gauge Demo include needle value reading, adjustable gauge angle, major value marks.

--- org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/sandbox/GaugeDemo.java
+++ org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/sandbox/GaugeDemo.java
@@ -38,15 +38,25 @@ import javafx.stage.Stage;
 public class GaugeDemo extends Application
 {
     //final private Canvas canvas = DemoHelper.createCanvas();
-    final private Canvas canvas = new Canvas (600, 400);
+    final private Canvas canvas = new Canvas (800, 600);
     private final AtomicLong counter = new AtomicLong();
-    private final Text updates = new Text("0");
+    private final Text NeedleRead = new Text("0");
     private final Polygon needle = new Polygon();
 
-    Double[] needle_pts = new Double[]{ -5.0, 0.0, 5.0, 0.0, 0.0, -50.0 };
-    Double needle_rotation = 45.0;
+    final int gauge_diam = 300;
+    final int gauge_major_len = gauge_diam / 10;
+    final int guage_minor_len = gauge_diam / 20;
+
+    Double gauge_total_ang = 220.0;
+    Double gauge_min = 0.0, gauge_max = 80.0;
+    Double gauge_major_interval = 10.0, gauge_minor_interval = 1.0;
+
+    Double[] needle_pts = new Double[]{ -5.0, 0.0, 5.0, 0.0, 0.0, -(gauge_diam / 2.0) };
+    Double needle_rotation = 0.0;
+    Double needle_direction = 1.0;
+    Double needle_value = 0.0;
     final Rotate rotationTransform = new Rotate(0, 0, 0);
-    final Translate xlateTransform = new Translate(0, -25);
+    final Translate xlateTransform = new Translate(0, -(gauge_diam / 4));
 
     Double gauge_fade = 0.0;
 
@@ -63,7 +73,7 @@ public class GaugeDemo extends Application
     public void start(final Stage stage)
     {
         final Label label1 = new Label("Canvas:");
-        final Label label2 = new Label("Updates:");
+        final Label label2 = new Label("Needle Value:");
 
         Text text = new Text("Hello");
         text.setFill(Color.RED);
@@ -76,7 +86,7 @@ public class GaugeDemo extends Application
         StackPane pane = makeStackPane();
         //AnchorPane pane = makeAnchorPane();
 
-        final VBox root_jfxnode = new VBox(label1, pane, label2, updates);
+        final VBox root_jfxnode = new VBox(label1, pane, label2, NeedleRead);
 
         final Scene scene = new Scene(root_jfxnode, 800, 700);
         stage.setScene(scene);
@@ -126,7 +136,6 @@ public class GaugeDemo extends Application
         gc.clearRect(0, 0, width, height);
         gc.drawRect(0, 0, width, height);
 
-        needle_rotation = (needle_rotation > 359) ? 0 : needle_rotation + 1.0;
         gauge_fade = (gauge_fade > 0.5) ? gauge_fade - 0.01 : 1.0;
         Double gauge_red = (100.0 * gauge_fade) / 255.0;
         Double gauge_green = (100.0 * gauge_fade) / 255.0;
@@ -144,18 +153,74 @@ public class GaugeDemo extends Application
         //final int x = (int)(Math.random() * (width-size));
         //final int y = (int)(Math.random() * (height-size));
 
-        final int size = 100;
+        final int size = gauge_diam;
         final int x = (width / 2) - (size / 2);
         final int y = (height / 2) - (size / 2);
         //gc.fillOval(x, y, size, size);
-        gc.fillArc(x, y, size, size, 0, 180);
+        final int start_arc_ang = (int) (90 - (gauge_total_ang / 2));
+        gc.fillArc(x, y, size, size, start_arc_ang, gauge_total_ang.intValue());
+
+        gc.setColor(java.awt.Color.GRAY);
+        java.awt.Font font = new java.awt.Font("Serif", java.awt.Font.PLAIN, 18);
+        gc.setFont(font);
+        gc.drawString("Text at upper left of gauge", x, y);
+        gc.drawLine(x, y, x+size, y);
+        gc.drawLine(x+size, y, x+size, y+size);
+
+        gc.setColor(java.awt.Color.PINK);
+        gc.drawLine(x+size, y+size, x, y+size);
+        gc.drawLine(x, y+size, x, y);
+        gc.drawString("Text at lower left of gauge", x+size, y+size);
+
+        String center_str = "Text Centered Under Gauge, Under Text at LL";
+        int textw = gc.getFontMetrics().stringWidth(center_str);
+        int texth = gc.getFontMetrics().getHeight();
+        int textx = x + (size / 2) - (textw / 2);
+        int texty = y + size + texth;
+        gc.drawString(center_str, textx, texty);
+
+
+        int xc = x + (size / 2);
+        int yc = y + (size / 2);
+        int r_outer = size / 2;
+        int r_inner = r_outer - gauge_major_len;
+        int total_major_marks = (int) Math.floor((gauge_max - gauge_min) / gauge_major_interval);
+        double alpha_ang = Math.toRadians(gauge_total_ang / total_major_marks);
+        double start_ang = Math.toRadians(start_arc_ang + 180);
+        for (int rdx = 0; rdx <= total_major_marks; rdx++)
+        {
+            double cos_ang = Math.cos(start_ang + alpha_ang * rdx);
+            double sin_ang = Math.sin(start_ang + alpha_ang * rdx);
+            int xo = (int) (xc + (r_outer * cos_ang));
+            int yo = (int) (yc + (r_outer * sin_ang));
+            int xi = (int) (xc + (r_inner * cos_ang));
+            int yi = (int) (yc + (r_inner * sin_ang));
+            gc.drawLine(xo, yo, xi, yi);
+            gc.drawString(String.valueOf(rdx), xo, yo);
+        }
+
+
+        needle_value = needle_value + needle_direction;
+
+        if (needle_value < 0)
+        {
+            needle_value = 0.0;
+            needle_direction = 1.0;
+        }
+        if (needle_value > gauge_max)
+        {
+            needle_value = gauge_max;
+            needle_direction = -1.0;
+        }
+
+        needle_rotation = start_arc_ang - 90 + ((needle_value) * (gauge_total_ang / (gauge_max - gauge_min)));
     }
 
     private void update_all(BufferedImage buf, WritableImage image, Semaphore done)
     {
         SwingFXUtils.toFXImage(buf, image);
         canvas.getGraphicsContext2D().drawImage(image, 0, 0);
-        updates.setText(Long.toString(counter.get()));
+        NeedleRead.setText(Double.toString(needle_value));
 
         //needle.setRotate(needle_rotation);
         rotationTransform.setAngle(needle_rotation);
