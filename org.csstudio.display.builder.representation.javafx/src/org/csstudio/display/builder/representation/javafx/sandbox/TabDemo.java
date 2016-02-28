5bd6693b97aed70a76cee16a2d366d073ab91af4
Kay <Kay.Kasemir@gmail.com>
2016-02-28 08:42:11 -0500

    Tab demo: Clarify comments

--- org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/sandbox/TabDemo.java
+++ org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/sandbox/TabDemo.java
@@ -7,23 +7,51 @@
  *******************************************************************************/
 package org.csstudio.display.builder.representation.javafx.sandbox;
 
-import java.util.concurrent.TimeUnit;
-
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.geometry.Side;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-/** Tabs..
+/** Tab demo
+ * 
+ *  Goal:
+ *  Bunch of widgets, including a Tab pane,
+ *  inside a scrollable and zoomable view.
+ *  
+ *  Basic idea:
+ *  ScrollPane[ Group[ widgets .. including TabPane ] ]
+ * 
+ *  ScrollPane - Obviously needed for scrolling
+ *  Group - Holds all the widgets and supports zooming
+ *  
+ *  Problem: 
+ *  ScrollPane shows scroll bars based on the original size of the widgets,
+ *  not based on the actual size.
+ *  https://pixelduke.wordpress.com/2012/09/16/zooming-inside-a-scrollpane
+ *  explains how to solve that by adding another nested Group:
+ * 
+ *  ScrollPane[ Group[ Group[ widgets .. including TabPane ] ] ]
+ * 
+ *  ScrollPane - Obviously needed for scrolling
+ *  outer Group - Automatically gets the layout bounds of the zoomed
+ *                widgets in the inner group, so ScrollPane
+ *                can correctly configure the scroll bars
+ *  inner Group - Holds all the widgets and supports zooming
+ *  
+ *  .. but now a TabPane will fail to properly draw itself
+ *  unless it's somehow forced to refresh, for example by toggling
+ *  the 'side' property.
+ *  
  *  @author Kay Kasemir
  */
 @SuppressWarnings("nls")
@@ -32,7 +60,7 @@ public class TabDemo extends Application
     @Override
     public void start(final Stage stage)
     {
-        // TabPane with some tabs, fixed size
+        // TabPane with some tabs
         final TabPane tabs = new TabPane();
         tabs.setStyle("-fx-background-color: red;");
         for (int i=0; i<3; ++i)
@@ -47,17 +75,13 @@ public class TabDemo extends Application
         tabs.setMinSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
         tabs.setPrefSize(400, 300);
 
-        // https://pixelduke.wordpress.com/2012/09/16/zooming-inside-a-scrollpane :
-        // To allow zooming of widgets in 'model_parent',
-        // it needs to be wrapped in another 'scroll_content' Group.
-        // Otherwise scroll bars would enable/disable based on layout bounds,
-        // regardless of zoom.
-        final Group model_parent = new Group(tabs);
-        model_parent.setScaleX(0.8);
-        model_parent.setScaleY(0.8);
-        final Group scroll_content = new Group(model_parent);
+        final Group widgets = new Group(tabs);
+        widgets.setScaleX(0.5);
+        widgets.setScaleY(0.5);
+        final Group scroll_content = new Group(widgets);
         final ScrollPane scroll = new ScrollPane(scroll_content);
         final Scene scene = new Scene(scroll);
+        stage.setTitle("Tab Demo");
         stage.setScene(scene);
         stage.show();
 
@@ -66,19 +90,15 @@ public class TabDemo extends Application
         // While the red background shows the area occupied by TabPane,
         // the actual Tabs are missing..
         System.out.println("See anything?");
-        new Thread(() ->
-        {
-            try
-            {   TimeUnit.SECONDS.sleep(4); }
-            catch (Exception e) {}
-            Platform.runLater(() ->
-            {   // .. until TabPane 'side' or tabMinWidth or .. properties
-                // are twiddled to force a refresh
-                tabs.setSide(Side.BOTTOM);
-                tabs.setSide(Side.TOP);
-                System.out.println("See it now?");
-           });
-        }).start();
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, (KeyEvent event) ->
+		{
+			if (event.getCode() == KeyCode.SPACE)
+	        {   // .. until 'side' or 'tabMinWidth' or .. are twiddled to force a refresh
+	            tabs.setSide(Side.BOTTOM);
+	            tabs.setSide(Side.TOP);
+	            System.out.println("See it now?");
+	        }
+		});
     }
 
     public static void main(String[] args)
