ab191ca1d84f6f10e0edafc74ba25f544e1d4e55
Megan Grodowitz <grodowitzml@ornl.gov>
2016-03-09 11:10:38 -0500

    Added initial BoolButton Widget model

---
+++ org.csstudio.display.builder.model/src/org/csstudio/display/builder/model/widgets/BoolButtonWidget.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2016 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.csstudio.display.builder.model.widgets;
+
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.behaviorPVName;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.displayFont;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.displayText;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.widgetMacros;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.csstudio.display.builder.model.Widget;
+import org.csstudio.display.builder.model.WidgetCategory;
+import org.csstudio.display.builder.model.WidgetDescriptor;
+import org.csstudio.display.builder.model.WidgetProperty;
+import org.csstudio.display.builder.model.macros.Macros;
+import org.csstudio.display.builder.model.persist.NamedWidgetFonts;
+import org.csstudio.display.builder.model.properties.WidgetFont;
+
+/** Widget that provides button for making a binary change
+ *  @author Megan Grodowitz
+ */
+@SuppressWarnings("nls")
+public class BoolButtonWidget extends Widget
+{
+    /** Widget descriptor */
+    public static final WidgetDescriptor WIDGET_DESCRIPTOR =
+        new WidgetDescriptor("action_button", WidgetCategory.CONTROL,
+            "Action Button",
+            "platform:/plugin/org.csstudio.display.builder.model/icons/action_button.png",
+            "Button that can open related displays or write PVs",
+            Arrays.asList("org.csstudio.opibuilder.widgets.ActionButton"))
+    {
+        @Override
+        public Widget createWidget()
+        {
+            return new BoolButtonWidget();
+        }
+    };
+
+    private volatile WidgetProperty<Macros> macros;
+    private volatile WidgetProperty<String> text;
+    private volatile WidgetProperty<WidgetFont> font;
+
+    public BoolButtonWidget()
+    {
+        super(WIDGET_DESCRIPTOR.getType());
+    }
+
+    @Override
+    protected void defineProperties(final List<WidgetProperty<?>> properties)
+    {
+        super.defineProperties(properties);
+        properties.add(behaviorPVName.createProperty(this, ""));
+        properties.add(macros = widgetMacros.createProperty(this, new Macros()));
+        properties.add(text = displayText.createProperty(this, "$(actions)"));
+        properties.add(font = displayFont.createProperty(this, NamedWidgetFonts.DEFAULT));
+    }
+
+    /** @return Widget 'macros' */
+    public WidgetProperty<Macros> widgetMacros()
+    {
+        return macros;
+    }
+
+    /** @return Display 'text' */
+    public WidgetProperty<String> displayText()
+    {
+        return text;
+    }
+
+    /** @return Display 'font' */
+    public WidgetProperty<WidgetFont> displayFont()
+    {
+        return font;
+    }
+
+    /** Action button widget extends parent macros
+     *  @return {@link Macros}
+     */
+    @Override
+    public Macros getEffectiveMacros()
+    {
+        final Macros base = super.getEffectiveMacros();
+        final Macros my_macros = widgetMacros().getValue();
+        return Macros.merge(base, my_macros);
+    }
+}
