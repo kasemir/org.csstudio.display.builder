daac3200bcb8a87f645b725bb344157c51e53ecb
kasemir <kasemirk@ornl.gov>
2016-04-25 12:23:32 -0400

    XYPlot: Represent multiple Y axes

--- org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/widgets/plots/XYPlotRepresentation.java
+++ org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/widgets/plots/XYPlotRepresentation.java
@@ -14,6 +14,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 
 import org.csstudio.display.builder.model.DirtyFlag;
+import org.csstudio.display.builder.model.UntypedWidgetPropertyListener;
 import org.csstudio.display.builder.model.WidgetProperty;
 import org.csstudio.display.builder.model.widgets.XYPlotWidget;
 import org.csstudio.display.builder.model.widgets.XYPlotWidget.AxisWidgetProperty;
@@ -38,6 +39,12 @@ public class XYPlotRepresentation extends RegionBaseRepresentation<Pane, XYPlotW
     private final DirtyFlag dirty_position = new DirtyFlag();
     private final DirtyFlag dirty_config = new DirtyFlag();
 
+    private final UntypedWidgetPropertyListener config_listener = (WidgetProperty<?> property, Object old_value, Object new_value) ->
+    {
+        dirty_config.mark();
+        toolkit.scheduleUpdate(this);
+    };
+
     /** Canvas that displays the image. */
     private RTValuePlot plot;
 
@@ -58,23 +65,58 @@ public class XYPlotRepresentation extends RegionBaseRepresentation<Pane, XYPlotW
     protected void registerListeners()
     {
         super.registerListeners();
-        model_widget.behaviorLegend().addUntypedPropertyListener(this::configChanged);
-        model_widget.behaviorXAxis().title().addUntypedPropertyListener(this::configChanged);
-        model_widget.behaviorXAxis().minimum().addUntypedPropertyListener(this::configChanged);
-        model_widget.behaviorXAxis().maximum().addUntypedPropertyListener(this::configChanged);
-        model_widget.behaviorXAxis().autoscale().addUntypedPropertyListener(this::configChanged);
-        model_widget.positionWidth().addUntypedPropertyListener(this::positionChanged);
-        model_widget.positionHeight().addUntypedPropertyListener(this::positionChanged);
+
+        model_widget.behaviorLegend().addUntypedPropertyListener(config_listener);
+
+        trackAxisChanges(model_widget.behaviorXAxis());
+
+        for (AxisWidgetProperty axis : model_widget.behaviorYAxes().getValue())
+            trackAxisChanges(axis);
+        model_widget.behaviorYAxes().addPropertyListener(this::yAxesChanged);
+
+        final UntypedWidgetPropertyListener position_listener = this::positionChanged;
+        model_widget.positionWidth().addUntypedPropertyListener(position_listener);
+        model_widget.positionHeight().addUntypedPropertyListener(position_listener);
 
         // TODO Handle multiple traces
+        // TODO model_widget.behaviorTraces().addPropertyListener(this::tracesChanged);
         model_widget.behaviorTraces().getElement(0).xValue().addUntypedPropertyListener(this::valueChanged);
         model_widget.behaviorTraces().getElement(0).yValue().addUntypedPropertyListener(this::valueChanged);
     }
 
-    private void configChanged(final WidgetProperty<?> property, final Object old_value, final Object new_value)
+    /** Listen to changed axis properties
+     *  @param axis X or Y axis
+     */
+    private void trackAxisChanges(final AxisWidgetProperty axis)
     {
-        dirty_config.mark();
-        toolkit.scheduleUpdate(this);
+        axis.title().addUntypedPropertyListener(config_listener);
+        axis.minimum().addUntypedPropertyListener(config_listener);
+        axis.maximum().addUntypedPropertyListener(config_listener);
+        axis.autoscale().addUntypedPropertyListener(config_listener);
+    }
+
+    /** Ignore changed axis properties
+     *  @param axis X or Y axis
+     */
+    private void ignoreAxisChanges(final AxisWidgetProperty axis)
+    {
+        axis.title().removePropertyListener(config_listener);
+        axis.minimum().removePropertyListener(config_listener);
+        axis.maximum().removePropertyListener(config_listener);
+        axis.autoscale().removePropertyListener(config_listener);
+    }
+
+    private void yAxesChanged(final WidgetProperty<List<AxisWidgetProperty>> property, final List<AxisWidgetProperty> removed, final List<AxisWidgetProperty> added)
+    {
+        if (removed != null)
+            for (AxisWidgetProperty axis : removed)
+                ignoreAxisChanges(axis);
+
+        if (added != null)
+            for (AxisWidgetProperty axis : added)
+                trackAxisChanges(axis);
+
+        config_listener.propertyChanged(property, removed, added);
     }
 
     private void positionChanged(final WidgetProperty<?> property, final Object old_value, final Object new_value)
@@ -142,11 +184,16 @@ public class XYPlotRepresentation extends RegionBaseRepresentation<Pane, XYPlotW
 
         // Update Y Axes
         final List<AxisWidgetProperty> model_y = model_widget.behaviorYAxes().getValue();
+        // Remove extra axes
+        int count = plot.getYAxes().size();
+        while (count > model_y.size())
+            plot.removeYAxis(--count);
+        // Add and/or adjust config of existing axes
         for (int i=0;  i<model_y.size();  ++i)
         {
             final AxisWidgetProperty model_axis = model_y.get(i);
             final YAxis<Double> plot_axis;
-            if (i <= plot.getYAxes().size())
+            if (i < plot.getYAxes().size())
             {
                 plot_axis = plot.getYAxes().get(i);
                 plot_axis.setName(model_axis.title().getValue());
