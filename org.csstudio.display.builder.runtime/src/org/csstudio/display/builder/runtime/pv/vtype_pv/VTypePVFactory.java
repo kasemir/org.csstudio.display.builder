b1c6e48af62ffb1a8f6f5a1bf979d865b67af02d
kasemir <kasemirk@ornl.gov>
2016-05-05 14:58:31 -0400

    PV: Patch 'longString' modifier for legacy PVM names

--- org.csstudio.display.builder.runtime/src/org/csstudio/display/builder/runtime/pv/vtype_pv/VTypePVFactory.java
+++ org.csstudio.display.builder.runtime/src/org/csstudio/display/builder/runtime/pv/vtype_pv/VTypePVFactory.java
@@ -18,10 +18,23 @@ import org.csstudio.vtype.pv.PVPool;
  */
 public class VTypePVFactory implements RuntimePVFactory
 {
+    /** @param name PV Name that might contain legacy information
+     *  @return Patched PV name
+     */
+    private String patch(String name)
+    {
+        // Remove PVManager's longString modifier.
+        // Not using regular expression because text
+        // had to be exactly like this at end of PV name.
+        if (name.endsWith(" {\"longString\":true}"))
+            name = name.substring(0,  name.length()-20);
+        return name;
+    }
+
     @Override
     public RuntimePV getPV(final String name) throws Exception
     {
-        return new VTypePV(PVPool.getPV(name));
+        return new VTypePV(PVPool.getPV(patch(name)));
     }
 
     @Override
