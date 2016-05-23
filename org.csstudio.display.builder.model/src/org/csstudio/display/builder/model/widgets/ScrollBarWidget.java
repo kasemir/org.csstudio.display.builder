73c57a0287ec6493a1810342eba4f7a0b18ff59a
Amanda Carpenter <cj5@ornl.gov>
2016-05-23 12:00:25 -0400

    ScrollBarWidget: added common properties (pv_name, minimum, maximum, & limits_from_pv)

--- org.csstudio.display.builder.model/src/org/csstudio/display/builder/model/widgets/ScrollBarWidget.java
+++ org.csstudio.display.builder.model/src/org/csstudio/display/builder/model/widgets/ScrollBarWidget.java
@@ -1,6 +1,10 @@
 package org.csstudio.display.builder.model.widgets;
 
-import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.displayBackgroundColor;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.behaviorLimitsFromPV;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.behaviorMaximum;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.behaviorMinimum;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.behaviorPVName;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.displayBorderAlarmSensitive;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,7 +13,6 @@ import org.csstudio.display.builder.model.Widget;
 import org.csstudio.display.builder.model.WidgetCategory;
 import org.csstudio.display.builder.model.WidgetDescriptor;
 import org.csstudio.display.builder.model.WidgetProperty;
-import org.csstudio.display.builder.model.properties.WidgetColor;
 
 /** Widget that can read/write numeric PV via scrollbar
  *  @author Amanda Carpenter
@@ -32,8 +35,15 @@ public class ScrollBarWidget extends VisibleWidget
         }
     };
 
-    private volatile WidgetProperty<WidgetColor> background;
-    //etc.
+
+
+    private volatile WidgetProperty<String> pv_name;
+    private volatile WidgetProperty<Double> minimum;
+    private volatile WidgetProperty<Double> maximum;
+    private volatile WidgetProperty<Boolean> limits_from_pv;
+    //display: show_value_tip, horizontal,
+    //behavior: bar_length (?? confusing description & behavior in old CSS)
+    //behavior: step_increment, page_increment
 
     public ScrollBarWidget()
     {
@@ -44,14 +54,35 @@ public class ScrollBarWidget extends VisibleWidget
     protected void defineProperties(final List<WidgetProperty<?>> properties)
     {
         super.defineProperties(properties);
-        properties.add(background = displayBackgroundColor.createProperty(this, new WidgetColor(30, 144, 255)));
-        //etc.
+        properties.add(pv_name = behaviorPVName.createProperty(this, ""));
+        properties.add(displayBorderAlarmSensitive.createProperty(this, true));
+        properties.add(minimum = behaviorMinimum.createProperty(this, 0.0));
+        properties.add(maximum = behaviorMaximum.createProperty(this, 0.0));
+        properties.add(limits_from_pv = behaviorLimitsFromPV.createProperty(this, false));
     }
 
-    /** @return Display 'background_color' */
-    public WidgetProperty<WidgetColor> displayBackgroundColor()
+    /** @return Behavior 'pv_name' */
+    public WidgetProperty<String> behaviorPVName()
     {
-        return background;
+        return pv_name;
     }
-    //etc.
+
+    /** @return Behavior 'minimum' */
+    public WidgetProperty<Double> behaviorMinimum()
+    {
+        return minimum;
+    }
+
+    /** @return Behavior 'maximum' */
+    public WidgetProperty<Double> behaviorMaximum()
+    {
+        return maximum;
+    }
+
+    /** @return Behavior 'limits_from_pv' */
+    public WidgetProperty<Boolean> behaviorLimitsFromPV()
+    {
+        return limits_from_pv;
+    }
+
 }
