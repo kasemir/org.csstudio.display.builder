22b91fe4130c47b1dd5fd0a394724b3117bfae9b
Amanda Carpenter <cj5@ornl.gov>
2016-06-10 16:33:20 -0400

    ComboWidget: added combo widget

---
+++ org.csstudio.display.builder.model/src/org/csstudio/display/builder/model/widgets/ComboWidget.java
@@ -0,0 +1,205 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2016 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.csstudio.display.builder.model.widgets;
+
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.behaviorPVName;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.displayBackgroundColor;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.displayBorderAlarmSensitive;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.displayFont;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.displayForegroundColor;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.displayText;
+import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.runtimeValue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.stream.XMLStreamWriter;
+
+import org.csstudio.display.builder.model.ArrayWidgetProperty;
+import org.csstudio.display.builder.model.Messages;
+import org.csstudio.display.builder.model.Widget;
+import org.csstudio.display.builder.model.WidgetCategory;
+import org.csstudio.display.builder.model.WidgetConfigurator;
+import org.csstudio.display.builder.model.WidgetDescriptor;
+import org.csstudio.display.builder.model.WidgetProperty;
+import org.csstudio.display.builder.model.WidgetPropertyCategory;
+import org.csstudio.display.builder.model.WidgetPropertyDescriptor;
+import org.csstudio.display.builder.model.ArrayWidgetProperty.ElementFactory;
+import org.csstudio.display.builder.model.persist.ModelReader;
+import org.csstudio.display.builder.model.persist.ModelWriter;
+import org.csstudio.display.builder.model.persist.NamedWidgetColors;
+import org.csstudio.display.builder.model.persist.NamedWidgetFonts;
+import org.csstudio.display.builder.model.persist.WidgetColorService;
+import org.csstudio.display.builder.model.persist.XMLUtil;
+import org.csstudio.display.builder.model.properties.CommonWidgetProperties;
+import org.csstudio.display.builder.model.properties.EnumWidgetProperty;
+import org.csstudio.display.builder.model.properties.WidgetColor;
+import org.csstudio.display.builder.model.properties.WidgetFont;
+import org.csstudio.display.builder.model.widgets.plots.PlotWidgetPointType;
+import org.diirt.vtype.VType;
+import org.osgi.framework.Version;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+/** Widget that writes to PV from selection of items
+ *  @author Amanda Carpenter
+ */
+@SuppressWarnings("nls")
+public class ComboWidget extends VisibleWidget
+{
+    /** Widget descriptor */
+    public static final WidgetDescriptor WIDGET_DESCRIPTOR =
+        new WidgetDescriptor("combo", WidgetCategory.CONTROL,
+            "ComboBox",
+            "platform:/plugin/org.csstudio.display.builder.model/icons/combo.gif",
+            "Writes one of a selection of options to a PV",
+            Arrays.asList("org.csstudio.opibuilder.widgets.MenuButton",
+                          "org.csstudio.opibuilder.widgets.combo"))
+    {
+        @Override
+        public Widget createWidget()
+        {
+            return new ComboWidget();
+        }
+    };
+    /** Custom configurator to read legacy *.opi files */
+    private static class ComboConfigurator extends WidgetConfigurator
+    {
+        public ComboConfigurator(final Version xml_version)
+        {
+            super(xml_version);
+        }
+
+        @Override
+        public boolean configureFromXML(final ModelReader model_reader, final Widget widget, final Element xml)
+                throws Exception
+        {
+            final String typeId = xml.getAttribute("typeId");
+            final boolean is_menu = typeId.equals("org.csstudio.opibuilder.widgets.MenuButton");
+            if (is_menu)
+            {
+                //Legacy Menu Buttons with actions from PV=false should be processed as action buttons, not combo boxes
+                final Element frompv_el = XMLUtil.getChildElement(xml, "actions_from_pv");
+                if ( frompv_el != null && !(XMLUtil.getString(frompv_el).equals("false")) )
+                    return false;
+
+                //Menu buttons used "actions_from_pv" instead of "items_from_pv"
+                if (frompv_el != null)
+                {
+                    final Document doc = xml.getOwnerDocument();
+                    Element items_from = doc.createElement(behaviorItemsFromPV.getName());
+
+                    if (frompv_el.getFirstChild() != null)
+                    {
+                        items_from.appendChild(frompv_el.getFirstChild().cloneNode(true));
+                    }
+                    else
+                    {
+                        items_from.appendChild(doc.createTextNode("true"));
+                    }
+                    xml.appendChild(items_from);
+                }
+                
+                //TODO: read in actions as items? or just remove actions, let rep. handle?
+            }
+
+            super.configureFromXML(model_reader, widget, xml);
+            return true;
+        }
+    }
+
+    @Override
+    public WidgetConfigurator getConfigurator(final Version persisted_version)
+            throws Exception
+    {
+        return new ComboConfigurator(persisted_version);
+    }
+    
+    /** Behavior 'item': element for list of 'items' property */
+    private static final WidgetPropertyDescriptor<String> behaviorItem =
+            CommonWidgetProperties.newStringPropertyDescriptor(WidgetPropertyCategory.BEHAVIOR, "item", Messages.ComboWidget_Item);
+
+    /** Behavior 'items': list of items (string properties) for combo box */
+    public static final WidgetPropertyDescriptor< List<WidgetProperty<String>> > behaviorItems =
+            new ArrayWidgetProperty.Descriptor< WidgetProperty<String> >(WidgetPropertyCategory.BEHAVIOR, "items", Messages.ComboWidget_Items,
+                                                                         (widget, index) -> behaviorItem.createProperty(widget, "Item " + index));
+
+    /** Behavior 'items_from_pv': If PV is enum PV, get items from PV? */
+    public static final WidgetPropertyDescriptor<Boolean> behaviorItemsFromPV =
+        CommonWidgetProperties.newBooleanPropertyDescriptor(WidgetPropertyCategory.BEHAVIOR, "items_from_pv", Messages.ComboWidget_ItemsFromPV);
+
+    private volatile WidgetProperty<String> pv_name;
+    private volatile WidgetProperty<WidgetColor> foreground;
+    private volatile WidgetProperty<WidgetColor> background;
+    private volatile WidgetProperty<WidgetFont> font;
+    private volatile WidgetProperty<VType> value;
+    private volatile WidgetProperty< List<WidgetProperty<String>> > items;
+    private volatile WidgetProperty<Boolean> items_from_pv;
+
+    public ComboWidget()
+    {
+        super(WIDGET_DESCRIPTOR.getType());
+    }
+
+    @Override
+    protected void defineProperties(final List<WidgetProperty<?>> properties)
+    {
+        super.defineProperties(properties);
+        properties.add(pv_name = behaviorPVName.createProperty(this, ""));
+        properties.add(displayBorderAlarmSensitive.createProperty(this, true));
+        properties.add(foreground = displayForegroundColor.createProperty(this, WidgetColorService.getColor(NamedWidgetColors.TEXT)));
+        properties.add(background = displayBackgroundColor.createProperty(this, WidgetColorService.getColor(NamedWidgetColors.READ_BACKGROUND)));
+        properties.add(font = displayFont.createProperty(this, NamedWidgetFonts.DEFAULT));
+        properties.add(value = runtimeValue.createProperty(this, null));
+        properties.add(items = behaviorItems.createProperty(this, null));
+        properties.add(items_from_pv = behaviorItemsFromPV.createProperty(this, true));
+    }
+
+    /** @return Behavior 'pv_name' */
+    public WidgetProperty<String> behaviorPVName()
+    {
+        return pv_name;
+    }
+
+    /** @return Display 'foreground_color' */
+    public WidgetProperty<WidgetColor> displayForegroundColor()
+    {
+        return foreground;
+    }
+
+    /** @return Display 'background_color' */
+    public WidgetProperty<WidgetColor> displayBackgroundColor()
+    {
+        return background;
+    }
+
+    /** @return Display 'font' */
+    public WidgetProperty<WidgetFont> displayFont()
+    {
+        return font;
+    }
+
+    /** @return Runtime 'value' */
+    public WidgetProperty<VType> runtimeValue()
+    {
+        return value;
+    }
+
+    /** @return Behavior 'items_from_PV' */
+    public WidgetProperty<Boolean> behaviorItemsFromPV()
+    {
+        return items_from_pv;
+    }
+
+    /** @return Behavior 'items' */
+    public WidgetProperty< List<WidgetProperty<String>> > behaviorItems()
+    {
+        return items;
+    }
+}
