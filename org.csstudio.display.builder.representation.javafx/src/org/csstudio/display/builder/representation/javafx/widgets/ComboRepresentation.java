49d2fd8ebb14a8a5905e51cb39361bd1f9fb3410
Amanda Carpenter <cj5@ornl.gov>
2016-06-13 15:06:16 -0400

    ComboRep.: adjust display of PV value; added example combo.bob

--- org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/widgets/ComboRepresentation.java
+++ org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/widgets/ComboRepresentation.java
@@ -12,6 +12,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.csstudio.display.builder.model.DirtyFlag;
 import org.csstudio.display.builder.model.WidgetProperty;
+import org.csstudio.display.builder.model.util.VTypeUtil;
 import org.csstudio.display.builder.model.widgets.ComboWidget;
 import org.csstudio.display.builder.representation.javafx.JFXUtil;
 import org.diirt.vtype.VEnum;
@@ -37,6 +38,7 @@ public class ComboRepresentation extends RegionBaseRepresentation<ComboBox<Strin
     private final DirtyFlag dirty_style = new DirtyFlag();
     private final DirtyFlag dirty_content = new DirtyFlag();
     private volatile List<String> items = new CopyOnWriteArrayList<String>();
+    private volatile int index = -1;
     private volatile Callback<ListView<String>,ListCell<String>> cellFactory = null;
 
     @Override
@@ -48,8 +50,10 @@ public class ComboRepresentation extends RegionBaseRepresentation<ComboBox<Strin
             String value = combo.getValue();
             if (value != null)
             {
+                // Restore current value
+                contentChanged(null, null, null);
+                // ... which should soon be replaced by updated value, if accepted
                 toolkit.fireWrite(model_widget, value);
-                combo.getEditor().setText(value);
             }
         });
         cellFactory = combo.getCellFactory();
@@ -107,27 +111,40 @@ public class ComboRepresentation extends RegionBaseRepresentation<ComboBox<Strin
     /** @param value Current value of PV
      *  @return Text to show, "<pv name>" if disconnected (no value)
      */
-    private List<String> computeItems(final VType value)
+    private List<String> computeItems(final VType value, final boolean fromPV)
     {
-        List<String> new_items = new CopyOnWriteArrayList<String>();
-        if (model_widget.behaviorItemsFromPV().getValue() && value instanceof VEnum)
+        if (fromPV)
         {
+            index = ((VEnum)value).getIndex();
             return ((VEnum)value).getLabels(); //TODO: is safe to return List, not CopyOnWriteArrayList?
         }
         else
         {
+            List<String> new_items = new CopyOnWriteArrayList<String>();
             List<WidgetProperty<String>> itemProps = model_widget.behaviorItems().getValue();
+            int new_index = -1;
+            String currValue = VTypeUtil.getValueString(value, false);
             for (WidgetProperty<String> itemProp : itemProps)
             {
                 new_items.add(itemProp.getValue());
+                if (itemProp.getValue().equals(currValue))
+                    new_index = new_items.size()-1;
             }
+            if (new_index < 0)
+            {
+                new_items.add(currValue);
+                new_index = items.size()-1;
+            }
+            index = new_index;
+            return new_items;
         }
-        return new_items;
     }
 
     private void contentChanged(final WidgetProperty<?> property, final Object old_value, final Object new_value)
     {
-        items = computeItems(model_widget.runtimeValue().getValue());
+        VType value = model_widget.runtimeValue().getValue();
+        boolean fromPV = model_widget.behaviorItemsFromPV().getValue() && value instanceof VEnum;
+        items = computeItems(value, fromPV); //also sets index
         dirty_content.mark();
         toolkit.scheduleUpdate(this);
     }
@@ -147,6 +164,9 @@ public class ComboRepresentation extends RegionBaseRepresentation<ComboBox<Strin
             }
         }
         if (dirty_content.checkAndClear())
+        {
             jfx_node.setItems(FXCollections.observableArrayList(items));
+            jfx_node.getSelectionModel().clearAndSelect(index);
+        }
     }
 }
