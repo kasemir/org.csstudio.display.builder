5a016bc4a39bda8b88efe96757924df4b6591b1f
kasemir <kasemirk@ornl.gov>
2016-06-27 12:24:41 -0400

    Scripts: Move all but script-accessible code to script.internal

--- org.csstudio.display.builder.runtime/src/org/csstudio/display/builder/runtime/script/internal/JythonScriptSupport.java
+++ org.csstudio.display.builder.runtime/src/org/csstudio/display/builder/runtime/script/internal/JythonScriptSupport.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.csstudio.display.builder.runtime.script;
+package org.csstudio.display.builder.runtime.script.internal;
 
 import static org.csstudio.display.builder.runtime.RuntimePlugin.logger;
 
