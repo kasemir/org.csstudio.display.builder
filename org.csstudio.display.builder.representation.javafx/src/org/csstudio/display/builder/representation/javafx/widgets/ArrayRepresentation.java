b35e4f668837150b60e9e54cbfadf5ed6f40a515
Amanda Carpenter <cj5@ornl.gov>
2016-07-19 09:51:14 -0400

    Array widget: infrastructure of representation

---
+++ org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/widgets/ArrayRepresentation.java
@@ -0,0 +1,16 @@
+package org.csstudio.display.builder.representation.javafx.widgets;
+
+import org.csstudio.display.builder.model.widgets.ArrayWidget;
+
+import javafx.scene.Node;
+
+public class ArrayRepresentation extends JFXBaseRepresentation<Node, ArrayWidget>
+{
+    @Override
+    protected Node createJFXNode() throws Exception
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
