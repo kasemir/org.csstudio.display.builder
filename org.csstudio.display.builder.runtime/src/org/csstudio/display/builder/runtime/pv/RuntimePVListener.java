b5f699bc56f4409a1406cc2ff24e59d078624779
kasemir <kasemirk@ornl.gov>
2016-07-25 14:50:48 -0400

    Runtime: Add array PV dispatcher..

--- org.csstudio.display.builder.runtime/src/org/csstudio/display/builder/runtime/pv/RuntimePVListener.java
+++ org.csstudio.display.builder.runtime/src/org/csstudio/display/builder/runtime/pv/RuntimePVListener.java
@@ -15,13 +15,24 @@ import org.diirt.vtype.VType;
  */
 public interface RuntimePVListener
 {
+    /** Notification from PV that indicates change in access permissions
+     *  @param pv PV that changed permissions
+     *  @param readonly Is PV now read-only, i.e. no write access?
+     */
     default public void permissionsChanged(RuntimePV pv, boolean readonly)
     {
         // Ignore
     }
 
+    /** Notification from PV that indicates change in value
+     *  @param pv PV that sent a new value
+     *  @param value Current value of the PV
+     */
     public void valueChanged(RuntimePV pv, VType value);
 
+    /** Notification from PV that indicates a disconnect
+     *  @param pv PV that is no longer accessible
+     */
     default public void disconnected(RuntimePV pv)
     {
         // Ignore
