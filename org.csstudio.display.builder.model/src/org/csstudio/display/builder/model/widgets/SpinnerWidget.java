08769725a6440d1426ef1007b5d06267713e583e
kasemir <kasemirk@ornl.gov>
2016-09-02 15:12:32 -0400

    Copyrights

--- org.csstudio.display.builder.model/src/org/csstudio/display/builder/model/widgets/SpinnerWidget.java
+++ org.csstudio.display.builder.model/src/org/csstudio/display/builder/model/widgets/SpinnerWidget.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2016 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.csstudio.display.builder.model.widgets;
 
 import static org.csstudio.display.builder.model.properties.CommonWidgetProperties.behaviorLimitsFromPV;
