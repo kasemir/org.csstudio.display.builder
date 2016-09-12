058828ae4864e86eac2407fcd72a84829edb84c9
Kay Kasemir <kasemirk@ornl.gov>
2016-09-12 10:44:40 -0400

    Audio: Linux vs. rest

--- org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/sandbox/AudioDemo.java
+++ org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/sandbox/AudioDemo.java
@@ -17,6 +17,7 @@ import javafx.stage.Stage;
 /** JFX Audio demo
  *  @author Kay Kasemir
  */
+@SuppressWarnings("nls")
 public class AudioDemo extends Application
 {
     public static void main(final String[] args)
@@ -30,7 +31,11 @@ public class AudioDemo extends Application
         stage.setTitle("Audio Demo");
         stage.show();
 
-        final File file = new File("../org.csstudio.display.builder.model/examples/audio/timer.wav");
+        // Windows and Mac OS X support WAV and MP3
+        // Linux: WAV hangs, MP3 results in MediaException for unsupported format
+        final File file =
+        //                new File("../org.csstudio.display.builder.model/examples/audio/timer.wav");
+                          new File("../org.csstudio.display.builder.model/examples/timer/timer.mp3");
         final Media audio = new Media(file.toURI().toString());
         final MediaPlayer player = new MediaPlayer(audio);
         player.setOnEndOfMedia( () -> player.stop());
