30f189c67f4f06aa6d741bb43de7a4bc84e00237
Megan Grodowitz <grodowitzml@ornl.gov>
2016-11-09 13:49:23 -0500

    Basic databrowser3 BOB widget working. Not all properties functional

--- databrowser3/databrowser-plugins/org.csstudio.trends.databrowser3.bobwidget/src/org/csstudio/trends/databrowser3/bobwidget/DataBrowserWidgetJFX.java
+++ databrowser3/databrowser-plugins/org.csstudio.trends.databrowser3.bobwidget/src/org/csstudio/trends/databrowser3/bobwidget/DataBrowserWidgetJFX.java
@@ -7,32 +7,62 @@
  ******************************************************************************/
 package org.csstudio.trends.databrowser3.bobwidget;
 
-import org.csstudio.display.builder.model.widgets.plots.XYPlotWidget;
+import java.io.InputStream;
+
+import org.csstudio.display.builder.model.DirtyFlag;
+import org.csstudio.display.builder.model.DisplayModel;
+import org.csstudio.display.builder.model.WidgetProperty;
+import org.csstudio.display.builder.model.macros.MacroHandler;
+import org.csstudio.display.builder.model.util.ModelResourceUtil;
+import org.csstudio.display.builder.model.util.ModelThreadPool;
 import org.csstudio.display.builder.representation.javafx.widgets.RegionBaseRepresentation;
+import org.csstudio.trends.databrowser3.model.Model;
+import org.csstudio.trends.databrowser3.persistence.XMLPersistence;
+import org.csstudio.trends.databrowser3.ui.JFXController;
 import org.csstudio.trends.databrowser3.ui.ModelBasedPlot;
 import org.eclipse.swt.widgets.Shell;
 
 import javafx.scene.layout.Pane;
+import javafx.geometry.Insets;
+import javafx.scene.layout.HBox;
 
 /** OPI Figure that displays data browser plot on screen,
  *  holds a Data Browser Plot
  *
  *  @author Kay Kasemir
  */
-public class DataBrowserWidgetJFX extends RegionBaseRepresentation<Pane, XYPlotWidget>
+public class DataBrowserWidgetJFX extends RegionBaseRepresentation<Pane, DataBrowserWidget>
 {
+    /** Change the position */
+    private final DirtyFlag dirty_pos = new DirtyFlag();
+    /** Change the size */
+    private final DirtyFlag dirty_size = new DirtyFlag();
+
     /** Data Browser plot */
-    private ModelBasedPlot plot;
+    private volatile ModelBasedPlot plot;
+
+    /** Model with data to display */
+    private volatile Model model = new Model();
+
+    private volatile JFXController controller;
+
+    private volatile String file_path;
 
     @Override
     public Pane createJFXNode() throws Exception
     {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #336699;");
         // Plot is only active in runtime mode, not edit mode
         //plot = new RTValuePlot(! toolkit.isEditMode());
         plot = new ModelBasedPlot(new Shell());
         //plot.showToolbar(false);
         //plot.showCrosshair(false);
         return plot.getPlot();
+
+        //return hbox;
     }
 
     /** @return Data Browser Plot */
@@ -41,33 +71,95 @@ public class DataBrowserWidgetJFX extends RegionBaseRepresentation<Pane, XYPlotW
         return plot;
     }
 
-    /** @return Tool bar visibility */
-    public boolean isToolbarVisible()
+    @Override
+    protected void registerListeners()
     {
-        return plot.getPlot().isToolbarVisible();
+        super.registerListeners();
+        controller = new JFXController(model, plot);
+
+        model_widget.propWidth().addUntypedPropertyListener(this::sizeChanged);
+        model_widget.propHeight().addUntypedPropertyListener(this::sizeChanged);
+        model_widget.propX().addUntypedPropertyListener(this::posChanged);
+        model_widget.propY().addUntypedPropertyListener(this::posChanged);
+
+        final String img_name = model_widget.propFile().getValue();
+        model_widget.propFile().addPropertyListener(this::fileChanged);
+        ModelThreadPool.getExecutor().execute(() -> fileChanged(null, null, img_name));
+
+        try
+        {
+            controller.start();
+        }
+        catch (Exception e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
-    /** @param visible New tool bar visibility */
-    public void setToolbarVisible(final boolean visible)
+    private void sizeChanged(final WidgetProperty<?> property, final Object old_value, final Object new_value)
     {
-        plot.getPlot().showToolbar(visible);
+        dirty_size.mark();
+        toolkit.scheduleUpdate(this);
     }
 
-    /** @return Legend visibility */
-    public boolean isLegendVisible()
+    private void posChanged(final WidgetProperty<?> property, final Object old_value, final Object new_value)
     {
-        return plot.getPlot().isLegendVisible();
+        dirty_pos.mark();
+        toolkit.scheduleUpdate(this);
     }
 
-    /** @param visible Legend visibility */
-    public void setLegendVisible(final boolean visible)
+    private void fileChanged(final WidgetProperty<String> property, final String old_value, final String new_value)
     {
-        plot.getPlot().showLegend(visible);
+        String base_path = new_value;
+        boolean load_failed = false;
+
+        try
+        {
+            // expand macros in the file name
+            final String expanded_path = MacroHandler.replace(model_widget.getMacrosOrProperties(), base_path);
+            // Resolve new image file relative to the source widget model (not 'top'!)
+            // Get the display model from the widget tied to this representation
+            final DisplayModel widget_model = model_widget.getDisplayModel();
+            // Resolve the path using the parent model file path
+            file_path = ModelResourceUtil.resolveResource(widget_model, expanded_path);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Failure resolving image path from base path: " + base_path);
+            e.printStackTrace();
+            load_failed = true;
+        }
+
+        if (!load_failed)
+        {
+            try
+            {
+                final InputStream stream = ModelResourceUtil.openResourceStream(file_path);
+                new XMLPersistence().load(model, stream);
+            }
+            catch (Exception e)
+            {
+                System.out.println("Failure loading plot file:" + file_path);
+                e.printStackTrace();
+                load_failed = true;
+            }
+        }
+        //toolkit.scheduleUpdate(this);
     }
 
-    /** @param showValueLabels <code>true</code> if values should be visible */
-    public void setShowValueLabels(final boolean showValueLabels)
+    @Override
+    public void updateChanges()
     {
-        plot.getPlot().showCrosshair(showValueLabels);
+        if (dirty_size.checkAndClear())
+        {
+            plot.getPlot().setPrefWidth(model_widget.propWidth().getValue());
+            plot.getPlot().setPrefHeight(model_widget.propHeight().getValue());
+        }
+        if (dirty_pos.checkAndClear())
+        {
+            plot.getPlot().setLayoutX(model_widget.propX().getValue());
+            plot.getPlot().setLayoutY(model_widget.propY().getValue());
+        }
     }
 }
---
+++ databrowser3/databrowser-plugins/org.csstudio.trends.databrowser3/src/org/csstudio/trends/databrowser3/ui/JFXController.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2015 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.csstudio.trends.databrowser3.ui;
+
+import java.time.Instant;
+import java.util.function.Consumer;
+
+import org.csstudio.trends.databrowser3.archive.ArchiveFetchJob;
+import org.csstudio.trends.databrowser3.model.Model;
+import org.csstudio.trends.databrowser3.model.PVItem;
+import org.eclipse.swt.widgets.Shell;
+
+import javafx.application.Platform;
+
+/** Controller that interfaces the {@link Model} with the {@link ModelBasedPlot}:
+ *  <ul>
+ *  <li>For each item in the Model, create a trace in the plot.
+ *  <li>Perform scrolling of the time axis.
+ *  <li>When the plot is interactively zoomed, update the Model's time range.
+ *  <li>Get archived data whenever the time axis changes.
+ *  </ul>
+ *  @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class JFXController extends ControllerBase
+{
+    final protected Shell shell = new Shell();
+
+    class JFXArchiveFetchJobListener extends BaseArchiveFetchJobListener
+    {
+        @Override
+        protected Shell getShell() { return shell; }
+
+        @Override
+        protected void executeOnUIThread(Consumer<Void> consumer)
+        {
+            Platform.runLater(() ->
+            {
+                consumer.accept(null);
+            });
+        }
+    };
+    final private JFXArchiveFetchJobListener archive_fetch_listener;
+
+    class JFXPlotListener extends BasePlotListener
+    {
+        @Override
+        protected Shell getShell() { return shell; }
+
+        @Override
+        protected void executeOnUIThread(Runnable func)
+        {
+            Platform.runLater(func);
+        }
+    };
+
+    @Override
+    protected ArchiveFetchJob makeArchiveFetchJob(PVItem pv_item, Instant start, Instant end)
+    {
+        return new ArchiveFetchJob(pv_item, start, end, archive_fetch_listener);
+    }
+
+    /** Initialize
+     *  @param shell Shell
+     *  @param model Model that has the data
+     *  @param plot Plot for displaying the Model
+     *  @throws Error when called from non-UI thread
+     */
+    public JFXController(final Model model, final ModelBasedPlot plot)
+    {
+        super (model, plot);
+        archive_fetch_listener = new JFXArchiveFetchJobListener();
+
+        // Listen to user input from Plot UI, update model
+        plot.addListener(new JFXPlotListener());
+    }
+
+}
