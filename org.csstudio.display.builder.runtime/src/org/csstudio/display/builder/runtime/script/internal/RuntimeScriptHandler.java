7a5fe31762f12091c7b7cd58e2e246cfca8e21c1
kasemir <kasemirk@ornl.gov>
2016-12-01 14:12:34 -0500

    Scripts: Only trigger when all PVs are connected
    Handles case where 'trigger' PVs have connected, but other PVs are
    missing. Once all connect, the last non-'trigger' PV will execute the
    script once. From then on, only trigger PVs result in further runs.
    
    PVs can still disconnect while the script is running.
    That's up to the script to handle.

--- org.csstudio.display.builder.runtime/src/org/csstudio/display/builder/runtime/script/internal/RuntimeScriptHandler.java
+++ org.csstudio.display.builder.runtime/src/org/csstudio/display/builder/runtime/script/internal/RuntimeScriptHandler.java
@@ -7,11 +7,13 @@
  *******************************************************************************/
 package org.csstudio.display.builder.runtime.script.internal;
 
+import static org.csstudio.display.builder.runtime.RuntimePlugin.logger;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.csstudio.display.builder.model.DisplayModel;
 import org.csstudio.display.builder.model.Widget;
@@ -43,10 +45,14 @@ public class RuntimeScriptHandler implements RuntimePVListener
     private final List<ScriptPV> infos;
     private final Script script;
 
-    /** 'pvs' is aligned with 'infos', i.e. pv[i] goes with infos.get(i) */
+    /** 'pvs' is aligned with 'infos', i.e. pvs[i] goes with infos.get(i) */
     private final RuntimePV[] pvs;
 
-    public final static Logger logger = Logger.getLogger(RuntimeScriptHandler.class.getName());
+    /** Is there a subscription to pvs[i]? */
+    private final AtomicBoolean[] subscribed;
+
+    /** Has script executed once? */
+    private final AtomicBoolean executed_once = new AtomicBoolean();
 
     /** Helper to compile script
      *
@@ -143,6 +149,7 @@ public class RuntimeScriptHandler implements RuntimePVListener
         this.infos = infos;
         this.script = script;
         pvs = new RuntimePV[infos.size()];
+        subscribed = new AtomicBoolean[infos.size()];
         createPVs();
     }
 
@@ -156,12 +163,13 @@ public class RuntimeScriptHandler implements RuntimePVListener
         {
             final String pv_name = MacroHandler.replace(macros, infos.get(i).getName());
             pvs[i] = PVFactory.getPV(pv_name);
+            subscribed[i] = new AtomicBoolean(true);
             runtime.addPV(pvs[i]);
         }
-        // Subscribe to trigger PVs
+        // Subscribe to all PVs.
+        // Will later unsubscribe from non-trigger PVs
         for (int i=0; i<pvs.length; ++i)
-            if (infos.get(i).isTrigger())
-                pvs[i].addListener(this);
+            pvs[i].addListener(this);
     }
 
     /** Must be invoked to dispose PVs */
@@ -170,7 +178,7 @@ public class RuntimeScriptHandler implements RuntimePVListener
         final WidgetRuntime<Widget> runtime = WidgetRuntime.ofWidget(widget);
         for (int i=0; i<pvs.length; ++i)
         {
-            if (infos.get(i).isTrigger())
+            if (subscribed[i].getAndSet(false))
                 pvs[i].removeListener(this);
             runtime.removePV(pvs[i]);
             PVFactory.releasePV(pvs[i]);
@@ -180,13 +188,50 @@ public class RuntimeScriptHandler implements RuntimePVListener
     @Override
     public void valueChanged(final RuntimePV pv, final VType value)
     {
-        /*
-        System.out.println(script + " triggered by " + pv + ":");
-        for (int i=0; i<pvs.length; ++i)
-            System.out.println(pvs[i] +
-                               (pvs[i].read() == null ? " (disconnected)" : " (connected)") +
-                               (infos.get(i).isTrigger() ? " - trigger!" : ""));
-        */
+        if (logger.isLoggable(Level.FINE))
+        {
+            final StringBuilder buf = new StringBuilder();
+            buf.append(script).append("\nTriggered by ").append(pv).append(" = ").append(value).append("\n");
+            for (int i=0; i<pvs.length; ++i)
+            {
+                buf.append("pvs[").append(i).append("]: ").append(pvs[i]).append(" = ").append(pvs[i].read());
+                if (infos.get(i).isTrigger())
+                    buf.append(" - trigger!");
+                buf.append("\n");
+            }
+            logger.fine(buf.toString());
+        }
+        // Skip script execution unless all PVs are connected
+        for (RuntimePV p : pvs)
+            if (p.read() == null)
+                return;
+
+        // Check if this PV is a 'trigger'
+        // (linear search, expecting only a few PVs per script)
+        int i;
+        for (i=0; i<pvs.length; ++i)
+            if (pv == pvs[i])
+                break;
+        if (i >= pvs.length)
+            throw new IllegalStateException(script + " triggered by unknown PV " + pv);
+
+        // If this is a trigger PV, execute the script.
+        // If not trigger PV,
+        // avoid further events from this PV
+        // and execute script only if this would be the first run.
+        // Scenario:
+        // All the 'trigger' PVs connected, but since non-trigger PVs
+        // were missing the script didn't execute.
+        // Finally all connect, and in this case the last connecting
+        // non-trigger PV actually triggers the (first) run.
+        if (! infos.get(i).isTrigger())
+        {
+            if (subscribed[i].getAndSet(false))
+                pvs[i].removeListener(this);
+            if (executed_once.getAndSet(true))
+                return;
+        }
+
         // Request execution of script
         script.submit(widget, pvs);
     }
