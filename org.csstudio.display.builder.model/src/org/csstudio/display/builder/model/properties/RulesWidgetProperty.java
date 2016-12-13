53f12fecef0b000b2592d2dc56c6c01891d47bf7
kasemir <kasemirk@ornl.gov>
2016-12-13 16:16:19 -0500

    Classes: Support for class-based rules
    Will assign _all_ rules of a widget, no longer allowing user to change
    them.

--- org.csstudio.display.builder.model/src/org/csstudio/display/builder/model/properties/RulesWidgetProperty.java
+++ org.csstudio.display.builder.model/src/org/csstudio/display/builder/model/properties/RulesWidgetProperty.java
@@ -12,7 +12,6 @@ import static org.csstudio.display.builder.model.ModelPlugin.logger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -87,13 +86,21 @@ public class RulesWidgetProperty extends WidgetProperty<List<RuleInfo>>
     {
         if (value instanceof RuleInfo[])
             setValue(Arrays.asList((RuleInfo[]) value));
-        else if ((value instanceof Collection) &&
-                ((Collection<?>)value).isEmpty())
-           setValue(Collections.emptyList());
+        else if (value instanceof Collection)
+        {
+            final List<RuleInfo> rules = new ArrayList<>();
+            for (Object item : (Collection<?>)value)
+            {
+                if (item instanceof RuleInfo)
+                    rules.add((RuleInfo) item);
+                else
+                    throw new Exception("Need RuleInfo[], got " + value);
+            }
+            setValue(rules);
+        }
         else
             throw new Exception("Need RuleInfo[], got " + value);
     }
-
 
     @Override
     public void writeToXML(final ModelWriter model_writer, final XMLStreamWriter writer) throws Exception
