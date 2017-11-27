c9ef7b1b6b0aa147177b69a0a54a9f6654456ce3
Kay Kasemir <kay.kasemir@gmail.com>
2017-11-27 10:12:01 -0500

    Remove dependency on org.controlsfx.control.textfield.TextFields

--- org.csstudio.display.builder.editor/src/org/csstudio/display/builder/editor/properties/PropertyPanel.java
+++ org.csstudio.display.builder.editor/src/org/csstudio/display/builder/editor/properties/PropertyPanel.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.controlsfx.control.textfield.TextFields;
 import org.csstudio.display.builder.editor.DisplayEditor;
 import org.csstudio.display.builder.model.DisplayModel;
 import org.csstudio.display.builder.model.Widget;
@@ -30,40 +29,37 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 
-
-/**
- * Property UI
+/** Property UI
  *
- * @author Kay Kasemir
- * @author Claudio Rosati
+ *  @author Kay Kasemir
+ *  @author Claudio Rosati
  */
 @SuppressWarnings( "nls" )
-public class PropertyPanel extends BorderPane {
-
+public class PropertyPanel extends BorderPane
+{
     private final DisplayEditor        editor;
     private final PropertyPanelSection section;
 
-    /**
-     * @param selection Selection handler
-     * @param undo 'Undo' manager
+    /** @param selection Selection handler
+     *  @param undo 'Undo' manager
      */
-    public PropertyPanel ( final DisplayEditor editor ) {
+    public PropertyPanel (final DisplayEditor editor)
+    {
 
         this.editor = editor;
         section = new PropertyPanelSection();
 
-        ScrollPane scrollPane = new ScrollPane();
-        HBox toolsPane = new HBox(6);
-        TextField searchField = TextFields.createClearableTextField();
-
+        final TextField searchField = new TextField();
         searchField.setPromptText("Search");
         searchField.textProperty().addListener( ( observable, oldValue, newValue ) -> filterProperties(newValue));
         HBox.setHgrow(searchField, Priority.NEVER);
 
+        final HBox toolsPane = new HBox(6);
         toolsPane.setAlignment(Pos.CENTER_RIGHT);
         toolsPane.setPadding(new Insets(6));
         toolsPane.getChildren().add(searchField);
 
+        final ScrollPane scrollPane = new ScrollPane();
         scrollPane.setFitToWidth(true);
         scrollPane.setContent(section);
         scrollPane.setMinHeight(0);
@@ -73,116 +69,106 @@ public class PropertyPanel extends BorderPane {
 
         // Track currently selected widgets
         editor.getWidgetSelectionHandler().addListener(this::setSelectedWidgets);
-        editor.getWidgetSelectionHandler().addListener(widgets -> {
+        editor.getWidgetSelectionHandler().addListener(widgets ->
+        {
             searchField.setDisable(widgets.isEmpty() && editor.getModel() == null);
             searchField.setText(null);
         });
 
         setMinHeight(0);
-
     }
 
-    public AutocompleteMenu getAutocompleteMenu ( ) {
+    public AutocompleteMenu getAutocompleteMenu ()
+    {
         return section.getAutocompleteMenu();
     }
 
-    /**
-     * Determine common properties
+    /** Determine common properties
      *
-     * @param primary Primary widget, the one selected first
-     * @param other Zero or more 'other' widgets
-     * @return Common properties
+     *  @param primary Primary widget, the one selected first
+     *  @param other Zero or more 'other' widgets
+     *  @return Common properties
      */
-    private Set<WidgetProperty<?>> commonProperties ( final Widget primary, final List<Widget> other ) {
+    private Set<WidgetProperty<?>> commonProperties(final Widget primary, final List<Widget> other)
+    {
 
-        if ( other.contains(primary) ) {
+        if (other.contains(primary))
             throw new IllegalArgumentException("Primary widget " + primary + " included in 'other'");
-        }
 
         // Start with properties of primary widget
         final Set<WidgetProperty<?>> common = new LinkedHashSet<>(primary.getProperties());
         // Keep properties shared by other widgets, i.e. remove those _not_ in
         // other
-        for ( Widget o : other ) {
+        for (Widget o : other)
             common.removeIf(prop -> !o.checkProperty(prop.getName()).isPresent());
-        }
 
         return common;
-
     }
 
-    private void filterProperties ( String search ) {
+    private void filterProperties(final String search)
+    {
+        final List<Widget> selection = editor.getWidgetSelectionHandler().getSelection();
 
-        List<Widget> selection = editor.getWidgetSelectionHandler().getSelection();
-
-        if ( search == null || search.trim().isEmpty() ) {
+        if (search == null || search.trim().isEmpty())
             setSelectedWidgets(selection);
-        } else {
-
+        else
+        {
             List<Widget> other;
             Set<WidgetProperty<?>> properties;
             final DisplayModel model = editor.getModel();
 
-            if ( selection.isEmpty() ) {
-                if ( model != null ) {
+            if (selection.isEmpty())
+            {
+                if (model != null)
+                {
                     other = Collections.emptyList();
                     properties = model.getProperties();
-                } else {
-                    return;
                 }
-            } else {
-
+                else
+                    return;
+            }
+            else
+            {
                 other = new ArrayList<>(selection);
-
                 final Widget primary = other.remove(0);
-
                 properties = commonProperties(primary, other);
-
             }
 
             properties = properties.stream()
-                    .filter(widgetProperty -> widgetProperty.getDescription().toLowerCase().contains(search.toLowerCase()))
-                    .collect(Collectors.toSet());
-
+                                   .filter(widgetProperty -> widgetProperty.getDescription().toLowerCase().contains(search.toLowerCase()))
+                                   .collect(Collectors.toSet());
             updatePropertiesView(properties, other);
-
         }
-
     }
 
-    /**
-     * Populate UI with properties of widgets
-     *
-     * @param widgets Widgets to configure
+    /** Populate UI with properties of widgets
+     *  @param widgets Widgets to configure
      */
-    private void setSelectedWidgets ( final List<Widget> widgets ) {
-
+    private void setSelectedWidgets(final List<Widget> widgets)
+    {
         final DisplayModel model = editor.getModel();
 
-        if ( widgets.isEmpty() ) {   // Use the DisplayModel
-            if ( model != null ) {
+        if (widgets.isEmpty())
+        {   // Use the DisplayModel
+            if (model != null)
                 updatePropertiesView(model.getProperties(), Collections.emptyList());
-            }
-        } else {   // Determine common properties
-
+        }
+        else
+        {   // Determine common properties
             final List<Widget> other = new ArrayList<>(widgets);
             final Widget primary = other.remove(0);
             final Set<WidgetProperty<?>> properties = commonProperties(primary, other);
 
             updatePropertiesView(properties, other);
-
         }
-
     }
 
-    private void updatePropertiesView ( final Set<WidgetProperty<?>> properties, final List<Widget> other ) {
-
+    private void updatePropertiesView(final Set<WidgetProperty<?>> properties, final List<Widget> other)
+    {
         final DisplayModel model = editor.getModel();
 
         section.clear();
         section.setClassMode(model != null && model.isClassModel());
         section.fill(editor.getUndoableActionManager(), properties, other);
-
     }
-
 }
