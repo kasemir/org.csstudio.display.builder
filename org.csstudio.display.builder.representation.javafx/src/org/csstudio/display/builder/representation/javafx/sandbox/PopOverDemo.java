79e68cb1deb9a274bfcd8003b33e53a196fe4b88
Kay Kasemir <kasemirk@ornl.gov>
2017-12-06 20:20:07 -0500

    Popover demo development...

--- org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/sandbox/PopOverDemo.java
+++ org.csstudio.display.builder.representation.javafx/src/org/csstudio/display/builder/representation/javafx/sandbox/PopOverDemo.java
@@ -11,9 +11,14 @@ import javafx.application.Application;
 import javafx.beans.InvalidationListener;
 import javafx.beans.WeakInvalidationListener;
 import javafx.geometry.Bounds;
+import javafx.geometry.Side;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.ObjectPropertyBase;
+
 import javafx.scene.control.Button;
+import javafx.scene.layout.Region;
 import javafx.scene.control.Label;
 import javafx.scene.control.PopupControl;
 import javafx.scene.control.Skin;
@@ -21,7 +26,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.ArcTo;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.HLineTo;
+import javafx.scene.shape.VLineTo;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
@@ -39,27 +49,76 @@ public class PopOverDemo extends Application
      */
     static class PopOverSkin implements Skin<PopOver>
     {
-        private final PopOver popover;
+        	private PopOver popover;
         private final StackPane root = new StackPane();
-        // TODO Turn into 'path' with pointer to 'reference'
-        private final Rectangle background = new Rectangle(200, 200,  Color.LIGHTGRAY);
 
+        // popover.content uses (0, 0) - (w, h)
+        // background extends around content by 'ROUND'
+        private final Path background = new Path();
+
+        /** Distance between edge of popover.actove_owner and popover.content */
+        static final double DISTANCE = 20.0;
+        
+        private static final double ROUND = 10.0;
+        
         private final InvalidationListener update_background = prop ->
         {
-            background.setWidth(root.getWidth());
-            background.setHeight(root.getHeight());
+            System.out.println("Updating size");
+            final double w = root.getWidth(),
+                         h = root.getHeight();
+            
+            switch (popover.side.get())
+            {
+            case TOP:
+                {   // popup is on top of owner, reference point is on bottom
+                    final double ref_x = popover.getOwner().getWidth() / 2;
+                    background.getElements().setAll(
+                            new MoveTo(0, -ROUND),
+                            new HLineTo(w),
+                            new ArcTo(ROUND, ROUND, 0, w+ROUND, 0, false, true),
+                            new VLineTo(h),
+                            new ArcTo(ROUND, ROUND, 0, w, h+ROUND, false, true),
+                            new HLineTo(ref_x + ROUND),
+                            new LineTo(ref_x, h+DISTANCE),
+                            new LineTo(ref_x-ROUND, h+ROUND),
+                            new HLineTo(0),
+                            new ArcTo(ROUND, ROUND, 0, -ROUND, h, false, true),
+                            new VLineTo(0),
+                            new ArcTo(ROUND, ROUND, 0, 0, -ROUND, false, true)); 
+                }
+                break;
+            case BOTTOM:
+                {   // popup is on buttom of owner, reference point is on top
+                    final double ref_x = popover.getOwner().getWidth() / 2;
+                    background.getElements().setAll(
+                            new MoveTo(0, -ROUND),
+                            new HLineTo(ref_x - ROUND),
+                            new LineTo(ref_x, -DISTANCE),
+                            new LineTo(ref_x+ROUND, -ROUND),
+                            new HLineTo(w),
+                            new ArcTo(ROUND, ROUND, 0, w+ROUND, 0, false, true),
+                            new VLineTo(h),
+                            new ArcTo(ROUND, ROUND, 0, w, h+ROUND, false, true),
+                            new HLineTo(0),
+                            new ArcTo(ROUND, ROUND, 0, -ROUND, h, false, true),
+                            new VLineTo(0),
+                            new ArcTo(ROUND, ROUND, 0, 0, -ROUND, false, true)); 
+                }
+                break;
+            }
         };
 
         PopOverSkin(final PopOver popover)
         {
             this.popover = popover;
             background.setManaged(false);
-            background.setArcWidth(10.0);
-            background.setArcHeight(10.0);
-            background.setStyle("-fx-fill: rgba(255.0,255.0,255.0, .95);" +
+            background.setStyle("-fx-stroke: linear-gradient(to bottom, rgba(0,0,0, .3), rgba(0, 0, 0, .7));" + 
+                                "-fx-stroke-width: 0.5;" + 
+                                "-fx-fill: rgba(255.0,255.0,255.0, .95);" +
                                 "-fx-effect: dropshadow(gaussian, rgba(0,0,0,.2), 10.0, 0.5, 2.0, 2.0);");
             root.widthProperty().addListener(update_background);
             root.heightProperty().addListener(update_background);
+            popover.side.addListener(update_background);
             root.getChildren().setAll(background, popover.getContentNode());
         }
 
@@ -96,13 +155,30 @@ public class PopOverDemo extends Application
         private final Node content;
 
         /** Node to which the popover is currently attached */
-        private Node active_owner;
+        private Region active_owner;
+        
+        /** Side of owner where popover is shown */
+        private ObjectPropertyBase<Side> side = new SimpleObjectProperty(Side.BOTTOM);
 
         private final InvalidationListener update_position = p ->
         {
             final Bounds bounds = active_owner.localToScreen(active_owner.getBoundsInLocal());
-            setAnchorX(bounds.getMinX());
-            setAnchorY(bounds.getMaxY());
+
+            switch (side.get())
+            {
+            case BOTTOM:
+                setAnchorLocation(AnchorLocation.WINDOW_TOP_LEFT);
+                setAnchorX(bounds.getMinX());
+                setAnchorY(bounds.getMaxY());
+                break;
+            case TOP:
+                setAnchorLocation(AnchorLocation.WINDOW_BOTTOM_LEFT);
+                setAnchorX(bounds.getMinX());
+                setAnchorY(bounds.getMinY());
+                break;
+            default:
+                break;
+            }
         };
         private final WeakInvalidationListener weak_update_position = new WeakInvalidationListener(update_position);
 
@@ -113,6 +189,11 @@ public class PopOverDemo extends Application
         public PopOver(final Node content)
         {
             this.content = content;
+        }
+
+        Region getOwner()
+        {
+            return active_owner;
         }
 
         Node getContentNode()
@@ -130,10 +211,11 @@ public class PopOverDemo extends Application
          *
          *  <p>Moving the node or window will result
          *  in move of the PopOver.
+         *  
          *  @param owner Owner node relative to which the PopOver will be located
          *  @see {@link PopupControl#hide()}
          */
-        public void show(final Node owner)
+        public void show(final Region owner)
         {
             // Unhook from previous owner
             if (active_owner != null)
@@ -141,6 +223,8 @@ public class PopOverDemo extends Application
                 final Window window = active_owner.getScene().getWindow();
                 window.xProperty().removeListener(weak_update_position);
                 window.yProperty().removeListener(weak_update_position);
+                active_owner.layoutXProperty().removeListener(weak_update_position);
+                active_owner.layoutYProperty().removeListener(weak_update_position);
             }
 
             // Track movement of owner resp. its window
@@ -148,13 +232,19 @@ public class PopOverDemo extends Application
             final Window window = owner.getScene().getWindow();
             window.xProperty().addListener(weak_update_position);
             window.yProperty().addListener(weak_update_position);
+            owner.layoutXProperty().addListener(weak_update_position);
+            owner.layoutYProperty().addListener(weak_update_position);
 
-            // TODO Determine 'reference',
-            // auto-position above/below/left/right of owner
-
+            // TODO Determine 'side',
+            // auto-position above/below/left/right of owner,
+            if (side.get() == Side.BOTTOM)
+                side.set(Side.TOP);
+            else
+                side.set(Side.BOTTOM);
+            
             // Show relative to owner
-            final Bounds bounds = owner.localToScreen(owner.getBoundsInLocal());
-            show(owner, bounds.getMinX(), bounds.getMaxY());
+            update_position.invalidated(null);
+            show(window);
         }
     }
 
@@ -171,6 +261,7 @@ public class PopOverDemo extends Application
                 popover.hide();
             else
                 popover.show(toggle_popup);
+                
         });
 
         final BorderPane layout = new BorderPane(toggle_popup);
