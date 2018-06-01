1289b626d034813e54e890bd16260741cf973f92
Claudio Rosati <Claudio.Rosati@esss.se>
2018-06-01 13:52:36 +0200

    Executing selection.clean() in the UI thread after the display model was set.

--- org.csstudio.display.builder.editor/src/org/csstudio/display/builder/editor/DisplayEditor.java
+++ org.csstudio.display.builder.editor/src/org/csstudio/display/builder/editor/DisplayEditor.java
@@ -555,6 +555,16 @@ public class DisplayEditor
         {
             logger.log(Level.SEVERE, "Error representing model", ex);
         }
+
+        // Bring up the model's properties.
+        try
+        {
+            toolkit.execute(() -> selection.clear());
+        }
+        catch (final Exception ex)
+        {
+            logger.log(Level.SEVERE, "Error selecting model", ex);
+        }
     }
 
     /** @return Currently edited model */
