bd27e4377c87dc1cb811ae5ca56c7b8503c14e96
Claudio Rosati <Claudio.Rosati@esss.se>
2019-01-24 12:58:42 +0100

    CSSTUDIO-923 #comment Overridden cancelEdit to call updateItem.

--- org.csstudio.display.builder.editor/src/org/csstudio/display/builder/editor/tree/WidgetTreeCell.java
+++ org.csstudio.display.builder.editor/src/org/csstudio/display/builder/editor/tree/WidgetTreeCell.java
@@ -111,4 +111,10 @@ class WidgetTreeCell extends TextFieldTreeCell<WidgetOrTab>
                 setGraphic(null);
         }
     }
+
+    @Override
+    public void cancelEdit ( ) {
+        super.cancelEdit();
+        updateItem(getItem(), false);
+    }
 }
