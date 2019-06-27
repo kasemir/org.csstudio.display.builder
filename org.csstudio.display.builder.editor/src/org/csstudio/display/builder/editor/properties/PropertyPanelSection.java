cbb26e1655099a2d0263601473447711e1fbf665
Claudio Rosati <Claudio.Rosati@esss.se>
2019-06-27 14:38:02 +0200

    Removed commented statement.

--- org.csstudio.display.builder.editor/src/org/csstudio/display/builder/editor/properties/PropertyPanelSection.java
+++ org.csstudio.display.builder.editor/src/org/csstudio/display/builder/editor/properties/PropertyPanelSection.java
@@ -335,7 +335,6 @@ public class PropertyPanelSection extends GridPane
                     if (combo.isVisible())
                     {
                         combo.requestFocus();
-//                        combo.getEditor().selectAll();
                     }
                     else if (check.isVisible())
                         check.requestFocus();
